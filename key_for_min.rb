107db3f49217ceccbefddb64452f5307ed1c5960
Katana Tran <tkatyk21@gmail.com>
2019-06-24 01:27:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  array = []
+  min_item = ""
+  min_price = (2**(0.size * 8 -2) -1)
+  name_hash.each do |item, price|
+    if price < min_price
+      min_item = item
+      min_price = price
+    end 
+  end 
+  min_item
 end
